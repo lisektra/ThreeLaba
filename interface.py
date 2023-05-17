c89f49508bbff5d50477b6443796aa70d59e432a
lisektra <106808462+lisektra@users.noreply.github.com>
2023-05-17 22:35:37 +0300

    Сохранение 2
    Исправление интерфейса

--- interface.py
+++ interface.py
@@ -13,6 +13,6 @@ open_button = tk.Button(root, text='Открыть вариант Литвино
 open_button.pack(padx=20, pady=10)
 open_button1 = tk.Button(root, text='Открыть вариант Николаевой', command=open_program)
 open_button1.pack(padx=20, pady=10)
-open_button2 = tk.Button(root, text='Открыть вариант Костина', command=open_program)
+open_button2 = tk.Button(root, text='Открыть вариант Костиной', command=open_program)
 open_button2.pack(padx=20, pady=10)
 root.mainloop()
