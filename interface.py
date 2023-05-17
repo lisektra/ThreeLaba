0f06c7b7dff5d88246821ffee99bb84e95fe18a5
Мария Николаева <106019319+MariaNicolaeva@users.noreply.github.com>
2023-05-17 23:37:07 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,14 +5,14 @@ def open_program():
     os.system('python crime.py')
 def open_program1():
     os.system('python population.py')
-def open_program1():
+def open_program2():
     os.system('python tourists.py')
 
 root = tk.Tk()
 open_button = tk.Button(root, text='Открыть вариант Литвиновой', command=open_program)
 open_button.pack(padx=20, pady=10)
-open_button1 = tk.Button(root, text='Открыть вариант Николаевой', command=open_program)
+open_button1 = tk.Button(root, text='Открыть вариант Николаевой', command=open_program1)
 open_button1.pack(padx=20, pady=10)
-open_button2 = tk.Button(root, text='Открыть вариант Костиной', command=open_program)
+open_button2 = tk.Button(root, text='Открыть вариант Костиной', command=open_program2)
 open_button2.pack(padx=20, pady=10)
 root.mainloop()
